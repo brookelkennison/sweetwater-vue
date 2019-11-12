3f5858f9018053d8790fa7d31933d1ba06a43430
Brooke Kennison <40898879+brookelkennison@users.noreply.github.com>
2019-11-12 16:54:38 -0600

    All products and information showing

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <div v-for="item in items" :key="item.id">
-      <h4>{{ item.productName }}</h4>
+    <div v-for="item in items" :key="item.id" style="margin: 30px; border: 2px solid black;">
+      <p>Manufacturer: {{ item.manufacturer }}</p>
+      <h1>
+        <a :href="item.url">{{ item.productName }}</a>
+      </h1>
       <img :src="item.image">
+      <h6>Description: {{ item.description }}</h6>
+      <p>Product Name: {{ item.productName }}</p>
+      <p>Product ID: {{ item.itemid }}</p>
+      <p>Quantity: {{ item.quantity }}</p>
+      <p>Available: {{ item.available }}</p>
+      <p>Price: ${{ item.price }}</p>
+      <button>Add to Cart</button>
     </div>
   </div>
 </template>
